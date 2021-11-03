94f0b80a07fda393d45a2548b8c1dfefceed554c
RogerOsaldeKsquare <Roger.Osalde@theksquaregroup.com>
2021-11-02 23:17:24 -0600

    Change Port Settings

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 // Modules
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT||5000;
 
 // Router
 const routes = require('./routes/router');
